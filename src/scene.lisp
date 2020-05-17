f66172e9934eb38a02114ccecbf6e9cf937a2e57
t-sin <shinichi.tanaka45@gmail.com>
2020-05-18 01:16:38 +0900

    Scenes have its resouces as hash table

--- src/scene.lisp
+++ src/scene.lisp
@@ -3,9 +3,12 @@
   (:export #:scene
            #:scene-width
            #:scene-height
+           #:scene-window
+           #:scene-resources
            #:draw
            #:update
-           #:input))
+           #:input
+           #:load-resources))
 (in-package #:lsd.scene)
 
 (defclass scene ()
@@ -14,8 +17,10 @@
           :accessor scene-width)
    (height :initform 600
            :initarg :height
-           :accessor scene-height)))
+           :accessor scene-height)
+   (resources :accessor scene-resources)))
 
+(defgeneric load-resources (scene renderer))
 (defgeneric draw (scene renderer))
 (defgeneric update (scene))
 (defgeneric input (scene &rest keys &key &allow-other-keys))
